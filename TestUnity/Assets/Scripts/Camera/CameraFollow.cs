d60d0f24796993d1400a97c908c6cbb52f1dc530
TBigLong <330373763@qq.com>
2018-10-10 23:59:10 +0800

    相机跟随

---
+++ TestUnity/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Vector3 offset;
+    public Transform player;
+
+    private void Awake()
+    {
+        offset = transform.position - player.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = player.position + offset;
+        transform.position = movement;
+    }
+}
